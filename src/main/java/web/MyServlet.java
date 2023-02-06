0bdd2f7fc156c7015bd70999f1bc8a7b84827239
ChebihiF <chebihi.f@ucd.ac.ma>
2023-02-06 11:54:43 +0100

    first commit

--- src/main/java/web/MyServlet.java
+++ src/main/java/web/MyServlet.java
@@ -1,2 +1,15 @@
-package web;public class MyServlet {
+package web;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class MyServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
 }
